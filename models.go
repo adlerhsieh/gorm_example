9ed4ef88f189a70ebbe4cb99e285c604ce5bb74c
Adler <nkj20932@hotmail.com>
2016-04-20 00:31:37 +0800

    add tags

--- models.go
+++ models.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"github.com/jinzhu/gorm"
+// "github.com/jinzhu/gorm"
 )
 
 type User struct {
-	gorm.Model
-	Username  string
+	ID        uint
+	Username  string `sql:"type:VARCHAR(255)"`
 	FirstName string
-	LastName  string
+	// Set default value
+	LastName string `sql:"DEFAULT:'Smith'"`
+	// ignored attribute will be treated as attr instead of column
+	IgnoredField bool `sql:"-"`
+	// Not Null & Unique field
+	UniqueField string `sql:"not null;unique"`
+
+	// Others
+
+	// auto-populate columns id and timestamps
+	// gorm.Model
+	// Custom primary ket
+	// UserID int `gorm:"primary_key"`
+	// Custom column name
+	// FirstName string `gorm:"column:FirstName"`
+	// AUTO_INCREMENT can only be set on key field
+	// Count     int `gorm:"AUTO_INCREMENT"`
 }
 
 var users []User = []User{
-	User{Username: "foobar", FirstName: "Foo", LastName: "Bar"},
-	User{Username: "helloworld", FirstName: "Hello", LastName: "World"},
-	User{Username: "john", FirstName: "John", LastName: "Smith"},
+	User{Username: "foobar", FirstName: "Foo", LastName: "Bar", UniqueField: "a"},
+	User{Username: "helloworld", FirstName: "Hello", LastName: "World", UniqueField: "b"},
+	User{Username: "john", FirstName: "John", UniqueField: "c"},
 }
